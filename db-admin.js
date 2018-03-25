4625e2918eb2d253314c7663393a34f05c138350
Sloan <gwaltneys2@winthrop.edu>
2018-03-24 23:41:11 -0400

    added db viewer neeeds code clean up

--- db-admin.js
+++ db-admin.js
@@ -1,25 +1,97 @@
 require('dotenv').config()
-const mongoose = require('mongoose')
+const mongojs = require('mongojs')
+const inquirer = require('inquirer')
 
-mongoose.connect(process.env.MONGO_URI)
+let db = mongojs(process.env.MONGO_URI)
+
+PromiseCollectionNames()
   .then(
-    () => { console.log('Welcome') },
-    err => { throw new Error(err) }
-  )
-  .catch(
-    err => { console.log(`DB Connection Error: ${err}`) }
+    (collection) => {
+      let collect = ''
+      let collectionQuestion = {
+        type: 'list',
+        name: 'collection',
+        message: 'Please Pick A Collection',
+        choices: collection
+      }
+      return inquirer.prompt([collectionQuestion])
+        .then(
+          (answer) => {
+            collect = answer.collection
+            return PromiseGetRecords(answer.collection)
+          }
+        )
+        .then(
+          (docs) => {
+            let collectionViewArr = []
+            docs.forEach(
+              (doc) => {
+                let viewObj = {}
+                let docKeys = Object.keys(doc)
+                viewObj[docKeys[0]] = doc[docKeys[0]]
+                viewObj[docKeys[1]] = doc[docKeys[1]]
+                collectionViewArr.push(JSON.stringify(viewObj))
+              }
+            )
+            return collectionViewArr
+          }
+        )
+        .then(
+          (viewArr) => {
+            let docQuestion = {
+              type: 'list',
+              message: 'Please Pick A Document To View',
+              name: 'doc',
+              choices: viewArr
+            }
+            inquirer.prompt([docQuestion])
+              .then(
+                (answer) => {
+                  let doc = JSON.parse(answer.doc)
+                  db[collect].findOne({_id: mongojs.ObjectId(doc._id)},
+                    (err, doc) => {
+                      if (err) {
+                        console.log(err)
+                      } else {
+                        console.log(doc)
+                      }
+                    }
+                  )
+                }
+              )
+          }
+        )
+    }
   )
 
-mongoose.connection.on('open',
-  (ref) => {
-    mongoose.connection.db.listCollections().toArray(
-      (err, names) => {
-        if (err) {
-          console.log('error getting collecton list')
-        } else {
-          console.log(names)
+function PromiseCollectionNames () {
+  return new Promise(
+    (resolve, reject) => {
+      db.getCollectionNames(
+        (err, names) => {
+          if (err) {
+            reject(err)
+          } else {
+            resolve(names)
+          }
         }
-      }
-    )
-  }
-)
+      )
+    }
+  )
+}
+
+function PromiseGetRecords (collection) {
+  return new Promise(
+    (resolve, reject) => {
+      db[collection].find({},
+        (err, docs) => {
+          if (err) {
+            reject(err)
+          } else {
+            resolve(docs)
+          }
+        }
+      )
+    }
+  )
+}
